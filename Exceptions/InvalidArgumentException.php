ef5e2a4cce3d70cede0d407d70c2aeb85e076e2e
Majid Abdolhosseini <majid8911303@gmail.com>
2016-03-30 16:45:48 +0430

    add InvalidArgumentException and complete smtp protocol class

---
+++ Exceptions/InvalidArgumentException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: majid
+ * Date: 3/30/16
+ * Time: 4:42 PM
+ */
+
+namespace mhndev\OOPMail\Exceptions;
+
+class InvalidArgumentException extends \InvalidArgumentException
+{
+
+}
--- Protocol/Smtp/Smtp.php
+++ Protocol/Smtp/Smtp.php
@@ -10,11 +10,19 @@ namespace mhndev\OOPMail\Protocol\Smtp;
 
 use iCommand;
 use mhndec\OOPMail\Interfaces\iTransport;
+use mhndev\OOPMail\Exceptions\InvalidArgumentException;
 use mhndev\OOPMail\Interfaces\iMessage;
 use mhndev\OOPMail\Protocol\AbstractProtocol;
 
 class Smtp extends AbstractProtocol implements iTransport
 {
+
+    /**
+     * The transport method for the socket
+     *
+     * @var string
+     */
+    protected $transport = 'tcp';
 
     /**
      * Indicates that a session is requested to be secure
@@ -23,9 +31,35 @@ class Smtp extends AbstractProtocol implements iTransport
      */
     protected $secure;
 
-    public function __construct()
+    public function __construct($host = '127.0.0.1', $port = null, array $config)
     {
+        if (isset($config['ssl'])) {
+            switch (strtolower($config['ssl'])) {
+                case 'tls':
+                    $this->secure = 'tls';
+                    break;
 
+                case 'ssl':
+                    $this->transport = 'ssl';
+                    $this->secure = 'ssl';
+                    if ($port === null) {
+                        $port = 465;
+                    }
+                    break;
+
+                default:
+                    throw new InvalidArgumentException($config['ssl'] . ' is unsupported SSL type');
+            }
+        }
+
+        // If no port has been specified then check the master PHP ini file. Defaults to 25 if the ini setting is null.
+        if ($port === null) {
+            if (($port = ini_get('smtp_port')) == '') {
+                $port = 25;
+            }
+        }
+
+        parent::__construct($host, $port);
     }
 
     /**
@@ -44,7 +78,7 @@ class Smtp extends AbstractProtocol implements iTransport
      */
     public function connect()
     {
-        // TODO: Implement connect() method.
+        return $this->_connect($this->transport . '://' . $this->host . ':' . $this->port);
     }
 
     /**
